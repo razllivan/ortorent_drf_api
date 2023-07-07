a9cb175278da34b234c28bbfe03c1b1aef0946e4
ivan <shanin-i2011@yandex.ru>
2023-07-07 22:18:03 +0300

    Refactor tests by replace check with ensure_connection

--- django_api/core/tests/test_commands.py
+++ django_api/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test custom Django management commands
 """
 
 from unittest.mock import patch
-
+from django.db import connection
 from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
@@ -11,25 +11,26 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch.object(connection, 'ensure_connection')
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
-    def test_wait_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, mocked_ensure_connection):
         """Test waiting for database if database ready"""
-        patched_check.return_value = True
+        mocked_ensure_connection.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=['default'])
+        mocked_ensure_connection.assert_called_once()
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, mocked_sleep, mocked_ensure_connection):
         """Test waiting for database when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
-                                    [OperationalError] * 3 + [True]
+        exceptions = [Psycopg2Error] * 2 + \
+                     [OperationalError] * 3 + \
+                     [True]
+        mocked_ensure_connection.side_effect = exceptions
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        self.assertEqual(mocked_ensure_connection.call_count, len(exceptions))
