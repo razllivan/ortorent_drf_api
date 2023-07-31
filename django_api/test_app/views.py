ce55e00d54feda60a99bb0ddff4540068e19189d
ivan <shanin-i2011@yandex.ru>
2023-07-31 18:15:16 +0300

    flake8 refactor

--- django_api/test_app/views.py
+++ django_api/test_app/views.py
@@ -1,6 +1,8 @@
 from test_app.models import TestModel
 from test_app.serializers import TestSerializer
 from rest_framework.viewsets import ModelViewSet
+
+
 class TestList(ModelViewSet):
     queryset = TestModel.objects.all()
     serializer_class = TestSerializer
