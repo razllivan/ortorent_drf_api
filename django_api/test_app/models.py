f1715dee7db8c6021b461b51201e10577406b50e
ivan <shanin-i2011@yandex.ru>
2023-08-08 22:21:39 +0300

    add field image to testmodel

--- django_api/test_app/models.py
+++ django_api/test_app/models.py
@@ -5,6 +5,7 @@ class TestModel(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return self.name
